1ed72585681ed1524ea43b546d9c3e91ebf27fa9
Mr. Will <mr.will.com@outlook.com>
2022-12-21 13:34:12 +0800

    Update theme config

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,16 +1,31 @@
 import { useRouter } from "next/router"
 
 export default {
+  docsRepositoryBase: 'https://github.com/MrWillCom/ChemWiki',
   logo: <span>ChemWiki</span>,
   project: {
     link: 'https://github.com/MrWillCom/ChemWiki',
   },
+  search: {
+    emptyResult: <div className="nx-mt-4 nx-flex nx-justify-center nx-items-center nx-text-gray-500 nx-font-semibold nx-text-s nx-select-none">没有结果</div>,
+    loading: '加载中...',
+    placeholder: '搜索',
+  },
   sidebar: {
     defaultMenuCollapseLevel: 0,
   },
-  head: <>
-    <link rel="stylesheet" href="https://unpkg.com/katex@0.16.4/dist/katex.min.css" />
-  </>,
+  toc: {
+    title: null,
+  },
+  editLink: {
+    text: '编辑本页面 ↗',
+  },
+  feedback: {
+    content: '有问题？前往反馈 ↗',
+  },
+  footer: {
+    text: <span>{new Date().getFullYear() === 2022 ? '2022' : '2022 - ' + new Date().getFullYear()} · By all <a href="https://github.com/MrWillCom/ChemWiki" target="_blank">ChemWiki</a> contributors with ❤️.</span>,
+  },
 
   useNextSeoProps() {
     const { route } = useRouter()
