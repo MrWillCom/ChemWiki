0aa6d06f932ef43b482318123671f5bc0284d38b
Mr. Will <mr.will.com@outlook.com>
2022-12-21 10:15:21 +0800

    Update `sidebar.defaultMenuCollapseLevel`

--- theme.config.jsx
+++ theme.config.jsx
@@ -5,6 +5,9 @@ export default {
   project: {
     link: 'https://github.com/MrWillCom/ChemWiki',
   },
+  sidebar: {
+    defaultMenuCollapseLevel: 0,
+  },
   head: <>
     <link rel="stylesheet" href="https://unpkg.com/katex@0.16.4/dist/katex.min.css" />
   </>,
