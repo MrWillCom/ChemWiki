23e699650d38ce9d9b3ac270942c7dafa3e6115a
Mr. Will <mr.will.com@outlook.com>
2022-12-22 08:32:36 +0800

    Add LXGW WenKai webfont to proj

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import "lxgw-wenkai-webfont/style.css";
 import "./styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
