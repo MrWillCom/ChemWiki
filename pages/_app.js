15a6ba040cb543cb3c46b3005fa460e210303ace
Mr. Will <mr.will.com@outlook.com>
2022-12-22 08:32:57 +0800

    Add React Flow

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import "lxgw-wenkai-webfont/style.css";
+import 'reactflow/dist/style.css';
 import "./styles.css";
 
 // This default export is required in a new `pages/_app.js` file.
