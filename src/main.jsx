43f7fc7d228869c0a7dd07c6a4569522b690f79a
Mr. Will <mr.will.com@outlook.com>
2023-02-04 11:07:20 +0800

    Add router and Sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import HomePage from './pages/Home'
+import './index.scss'
+import { ThemeProvider, BaseStyles } from '@primer/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BaseStyles>
+      <ThemeProvider colorMode="auto">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </BaseStyles>
   </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.module.scss'
+
+function HomePage() {
+  return (
+    <>
+      <h1>Working in Progress...</h1>
+    </>
+  )
+}
+
+export default HomePage
