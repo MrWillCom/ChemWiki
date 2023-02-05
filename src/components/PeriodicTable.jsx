7b5f7e6f881b441ce027457494dc2cd086e73021
Mr. Will <mr.will.com@outlook.com>
2023-02-05 11:52:40 +0800

    Update periodic table

--- src/components/PeriodicTable.jsx
+++ src/components/PeriodicTable.jsx
@@ -1,15 +1,17 @@
 import styles from './PeriodicTable.module.scss'
 import elements from '../data/elements'
+import { SegmentedControl, FormControl } from '@primer/react'
+import React from 'react'
 
-function generateTable() {
+function generateTable(symbol, selectedElement, setSelectedElement) {
   var table = [
     [1, 'spacer', 2],
     [[3, 4], 'spacer', [5, 10]],
     [[11, 12], 'spacer', [13, 18]],
     [[19, 36]],
     [[37, 54]],
-    [[55, 56], <Spacer label="57-71" noBorder />, [72, 86]],
-    [[87, 88], <Spacer label="89-103" noBorder />, [104, 118]],
+    [[55, 56], <Spacer label="57-71" highlight />, [72, 86]],
+    [[87, 88], <Spacer label="89-103" highlight />, [104, 118]],
     ['spacer', [57, 71]],
     ['spacer', [89, 103]],
   ]
@@ -18,10 +20,28 @@ function generateTable() {
     var newRow = []
     row.forEach(cell => {
       if (typeof cell === 'number') {
-        newRow.push(<Element atomicNumber={cell} />)
+        newRow.push(
+          <Element
+            atomicNumber={cell}
+            symbol={symbol}
+            active={cell === selectedElement}
+            onClick={() => {
+              setSelectedElement(cell)
+            }}
+          />,
+        )
       } else if (Array.isArray(cell)) {
         for (let i = cell[0]; i <= cell[1]; i++) {
-          newRow.push(<Element atomicNumber={i} />)
+          newRow.push(
+            <Element
+              atomicNumber={i}
+              symbol={symbol}
+              active={i === selectedElement}
+              onClick={() => {
+                setSelectedElement(i)
+              }}
+            />,
+          )
         }
       } else {
         newRow.push(cell)
@@ -44,31 +64,91 @@ function generateTable() {
   return table
 }
 
-function Row({ children }) {
-  return <div className={styles.row}>{children}</div>
+function Element({ atomicNumber, symbol, active, ...props }) {
+  const element = elements[atomicNumber - 1]
+
+  return (
+    <button
+      className={styles.cell + (active ? ' ' + styles.active : '')}
+      {...props}
+    >
+      <span className={styles.atomicNumber}>{element.atomicNumber}</span>
+      {symbol === 0 ? (
+        <span className={styles.symbol}>{element.symbol}</span>
+      ) : (
+        <span className={styles.name}>{element.name}</span>
+      )}
+      <span className={styles.atomicMass}>{element.atomicMass}</span>
+    </button>
+  )
 }
 
-function Element({ atomicNumber }) {
+function Spacer({ label, className, highlight, ...props }) {
   return (
-    <div className={styles.cell}>
-      {elements[atomicNumber - 1].symbol} {elements[atomicNumber - 1].name}
+    <div
+      className={
+        styles.spacer +
+        (className ? ' ' + className : '') +
+        (highlight ? ' ' + styles.highlight : '')
+      }
+      {...props}
+    >
+      {label}
     </div>
   )
 }
 
-function Spacer({ label }) {
-  return <div className={styles.spacer}>{label}</div>
+function ElementDisplay({ atomicNumber, className }) {
+  const element = elements[atomicNumber - 1]
+
+  return (
+    <div className={styles.elementDisplay + (className ? ' ' + className : '')}>
+      {JSON.stringify(element)}
+    </div>
+  )
 }
 
 function PeriodicTable() {
-  const table = generateTable()
+  const [selectedElement, setSelectedElement] = React.useState(1)
+
+  const [selectedSymbol, setSelectedSymbol] = React.useState(0)
+  const handleSymbolSegmentChange = selectedSymbol => {
+    setSelectedSymbol(selectedSymbol)
+  }
 
   return (
-    <div className={styles.table}>
-      {table.map(row => (
-        <Row>{row}</Row>
-      ))}
-    </div>
+    <>
+      <div className={styles.table}>
+        {generateTable(
+          selectedSymbol,
+          selectedElement,
+          setSelectedElement,
+        ).flat()}
+      </div>
+      <div className={styles.controls}>
+        <div className={styles.options}>
+          <FormControl>
+            <FormControl.Label>符号显示</FormControl.Label>
+            <SegmentedControl
+              aria-label="Symbol display"
+              size="small"
+              onChange={handleSymbolSegmentChange}
+            >
+              <SegmentedControl.Button selected={selectedSymbol === 0}>
+                元素符号
+              </SegmentedControl.Button>
+              <SegmentedControl.Button selected={selectedSymbol === 1}>
+                元素名称
+              </SegmentedControl.Button>
+            </SegmentedControl>
+          </FormControl>
+        </div>
+        <ElementDisplay
+          atomicNumber={selectedElement}
+          className={styles.display}
+        />
+      </div>
+    </>
   )
 }
 
