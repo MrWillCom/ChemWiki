01826d3ea16f2dc8ee69ff98efd0cf563acdd6d5
Mr. Will <mr.will.com@outlook.com>
2023-02-06 23:16:04 +0800

    Update navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,17 @@
 import styles from './NavBar.module.scss'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-function NavigationItem({ className, children, ...props }) {
+function NavigationItem({ className, children, match, ...props }) {
+  const router = useRouter()
+
   return (
     <Link
-      className={styles.navItem + (className ? ' ' + className : '')}
+      className={
+        styles.navItem +
+        (match && router.asPath.match(match) ? ' ' + styles.active : '') +
+        (className ? ' ' + className : '')
+      }
       {...props}
     >
       {children}
@@ -15,12 +22,24 @@ function NavigationItem({ className, children, ...props }) {
 function NavBar() {
   return (
     <nav className={styles.nav}>
-      <NavigationItem href="/">ChemWiki</NavigationItem>
-      <NavigationItem href="/definitions">定义</NavigationItem>
-      <NavigationItem href="/elements">元素</NavigationItem>
-      <NavigationItem href="/substances">物质</NavigationItem>
-      <NavigationItem href="/experiments">实验</NavigationItem>
-      <NavigationItem href="/appendix">附录</NavigationItem>
+      <NavigationItem href="/" match={/^\/$/}>
+        ChemWiki
+      </NavigationItem>
+      <NavigationItem href="/definitions" match={/^\/definitions/}>
+        定义
+      </NavigationItem>
+      <NavigationItem href="/elements" match={/^\/elements/}>
+        元素
+      </NavigationItem>
+      <NavigationItem href="/substances" match={/^\/substances/}>
+        物质
+      </NavigationItem>
+      <NavigationItem href="/experiments" match={/^\/experiments/}>
+        实验
+      </NavigationItem>
+      <NavigationItem href="/appendix" match={/^\/appendix/}>
+        附录
+      </NavigationItem>
     </nav>
   )
 }
