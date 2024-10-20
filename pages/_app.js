53cdcf12e462a6050bca348d65d10d3c6a49c8bc
Mr. Will <mr.will.com@outlook.com>
2024-10-20 11:36:21 +0800

    Remove reactflow

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
 import 'lxgw-wenkai-webfont/style.css'
-import 'reactflow/dist/style.css'
 import './styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
